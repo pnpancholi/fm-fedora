6e921186014f3e906ad08c97b09a7ec98b4b9c2d
pradhumna <pnpancholi@protonmail.com>
2026-09-20 17:08:34 +0530

    switch to different copr for hyprland on fedora

--- install/core/02-hyprland.sh
+++ install/core/02-hyprland.sh
@@ -6,8 +6,8 @@ source "$FM_FEDORA_PATH/lib/helpers.sh"
 
 info "Installing Hyprland core packages..."
 
-# Enable Hyprland COPR (required for hyprland, xdg-desktop-portal-hyprland, hyprpaper)
-enable_copr "solopasha/hyprland"
+# Enable Hyprland COPR (Fedora 43/44 support with vendored deps)
+enable_copr "ashbuk/Hyprland-Fedora"
 
 sudo dnf install -y hyprland xdg-desktop-portal-hyprland hyprpaper \
     polkit-gnome brightnessctl playerctl pavucontrol
